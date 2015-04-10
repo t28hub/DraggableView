de085a0f620d98f60d5d054ed9f403b8380df1bf
Tatsuya Maki <t28oogle@gmail.com>
2015-04-11 00:25:49 +0900

    サンプルにDraggableViewを追加

--- example/src/main/java/com/t28/draggableview/MainActivity.java
+++ example/src/main/java/com/t28/draggableview/MainActivity.java
@@ -2,8 +2,11 @@ package com.t28.draggableview;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.t28.draggablelistview.DraggableListView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DraggableListView listView = (DraggableListView) findViewById(R.id.main_container);
+        listView.setHasFixedSize(true);
+        listView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
