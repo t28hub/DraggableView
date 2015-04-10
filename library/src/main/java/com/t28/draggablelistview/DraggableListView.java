538be29592511d51102b118e18c76aa03dbeb775
Tatsuya Maki <t28oogle@gmail.com>
2015-04-10 20:54:57 +0900

    DraggableListViewの追加

---
+++ library/src/main/java/com/t28/draggablelistview/DraggableListView.java
@@ -0,0 +1,19 @@
+package com.t28.draggablelistview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+public class DraggableListView extends RecyclerView {
+    public DraggableListView(Context context) {
+        super(context);
+    }
+
+    public DraggableListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DraggableListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
