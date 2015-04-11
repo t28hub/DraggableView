8d9b6f9d7d313d579ff681b71c3a8cd76bd85800
Tatsuya Maki <t28oogle@gmail.com>
2015-04-11 15:28:03 +0900

    サンプル用のアダプタ実装

---
+++ example/src/main/java/com/t28/draggableview/ItemAdapter.java
@@ -0,0 +1,81 @@
+package com.t28.draggableview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.t28.draggablelistview.DraggableListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ItemAdapter extends DraggableListView.Adapter<ItemAdapter.ItemViewHolder> {
+    private final List<String> mItems;
+
+    public ItemAdapter(List<String> items) {
+        if (items == null || items.size() == 0) {
+            mItems = Collections.emptyList();
+        } else {
+            mItems = new ArrayList<>(items);
+        }
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        final View itemView = inflater.inflate(R.layout.layout_item, parent, false);
+        return new ItemViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        final String title = getItem(position);
+        holder.bind(title);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).hashCode();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public boolean move(int position1, int position2) {
+        final int limit = mItems.size();
+        if (position1 < 0 || position1 >= limit) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (position2 < 0 || position2 >= limit) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        mItems.add(position2, mItems.remove(position1));
+        notifyItemMoved(position1, position2);
+
+        return false;
+    }
+
+    private String getItem(int position) {
+        return mItems.get(position);
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+        private final TextView mTitleView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            mTitleView = (TextView) itemView.findViewById(R.id.item_title);
+        }
+
+        public void bind(String title) {
+            mTitleView.setText(title);
+        }
+    }
+}
