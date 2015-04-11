0be150e794c63949c6acbaccecb0f1a8e6f2ffb7
Tatsuya Maki <t28oogle@gmail.com>
2015-04-11 19:14:43 +0900

    LayoutManagerをGridLayoutManagerに変更

--- example/src/main/java/com/t28/draggableview/MainActivity.java
+++ example/src/main/java/com/t28/draggableview/MainActivity.java
@@ -2,7 +2,7 @@ package com.t28.draggableview;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,7 +23,7 @@ public class MainActivity extends ActionBarActivity implements ItemAdapter.OnIte
 
         mListView = (DraggableListView) findViewById(R.id.main_container);
         mListView.setHasFixedSize(true);
-        mListView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        mListView.setLayoutManager(new GridLayoutManager(this, 3));
 
         final List<String> dataSet = Arrays.asList(getResources().getStringArray(R.array.lineups));
         final ItemAdapter adapter = new ItemAdapter(dataSet);
