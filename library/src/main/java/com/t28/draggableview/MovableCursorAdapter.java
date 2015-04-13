e188c2be87aabdb8c1791bdddf5b6aa5455be9cf
Tatsuya Maki <t28oogle@gmail.com>
2015-04-14 00:39:29 +0900

    moveメソッドを仮実装

--- library/src/main/java/com/t28/draggableview/MovableCursorAdapter.java
+++ library/src/main/java/com/t28/draggableview/MovableCursorAdapter.java
@@ -41,6 +41,33 @@ public abstract class MovableCursorAdapter<VH extends RecyclerView.ViewHolder> e
 
     @Override
     public boolean move(int position1, int position2) {
+        if (!isValidPosition(position1)) {
+            throw new IndexOutOfBoundsException("'position1' is out of bounds:" + position1);
+        }
+        if (!isValidPosition(position2)) {
+            throw new IndexOutOfBoundsException("'position2' is out of bounds:" + position2);
+        }
+
+        if (position1 == position2) {
+            return false;
+        }
+
+        if (position1 < position2) {
+            moveLower(position1, position2);
+        } else {
+            moveUpper(position1, position2);
+        }
+        notifyItemMoved(position1, position2);
         return false;
+    }
+
+    protected boolean isValidPosition(int position) {
+        return position >= 0 && position < mCursor.getCount();
+    }
+
+    private void moveLower(int position1, int position2) {
+    }
+
+    private void moveUpper(int position1, int position2) {
     }
 }
