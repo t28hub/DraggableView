4c099273e03572a36915e9aafd924b60ec9a461e
Tatsuya Maki <t28oogle@gmail.com>
2015-04-15 01:01:12 +0900

    ContentObserverとDataSetObserverの生成メソッド追加

--- library/src/main/java/com/t28/draggableview/MovableCursorAdapter.java
+++ library/src/main/java/com/t28/draggableview/MovableCursorAdapter.java
@@ -1,16 +1,22 @@
 package com.t28.draggableview;
 
+import android.database.ContentObserver;
 import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.net.Uri;
+import android.os.Handler;
 import android.provider.BaseColumns;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseIntArray;
 
 public abstract class MovableCursorAdapter<VH extends RecyclerView.ViewHolder> extends DraggableView.Adapter<VH> {
     private final SparseIntArray mPositionMap;
+    private final ContentObserver mContentObserver;
+    private final DataSetObserver mDataSetObserver;
 
+    private boolean mIsDataValid;
     private int mRowIdColumn;
     private Cursor mCursor;
-
 
     public MovableCursorAdapter() {
         this(null);
@@ -18,6 +24,9 @@ public abstract class MovableCursorAdapter<VH extends RecyclerView.ViewHolder> e
 
     public MovableCursorAdapter(Cursor cursor) {
         mPositionMap = new SparseIntArray();
+        mContentObserver = createContentObserver();
+        mDataSetObserver = createDataSetObserver();
+
         if (cursor == null) {
             mCursor = new NullCursor();
         } else {
@@ -97,6 +106,9 @@ public abstract class MovableCursorAdapter<VH extends RecyclerView.ViewHolder> e
         return oldCursor;
     }
 
+    protected void onContentChanged() {
+    }
+
     protected boolean isValidPosition(int position) {
         return position >= 0 && position < mCursor.getCount();
     }
@@ -126,5 +138,40 @@ public abstract class MovableCursorAdapter<VH extends RecyclerView.ViewHolder> e
         final int cursorPosition2 = getCursorPosition(position2);
         setCursorPosition(position1, cursorPosition2);
         setCursorPosition(position2, cursorPosition1);
+    }
+
+    private ContentObserver createContentObserver() {
+        return new ContentObserver(new Handler()) {
+            @Override
+            public boolean deliverSelfNotifications() {
+                return true;
+            }
+
+            @Override
+            public void onChange(boolean selfChange) {
+                onContentChanged();
+            }
+
+            @Override
+            public void onChange(boolean selfChange, Uri uri) {
+                onContentChanged();
+            }
+        };
+    }
+
+    private DataSetObserver createDataSetObserver() {
+        return new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                mIsDataValid = true;
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onInvalidated() {
+                mIsDataValid = false;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
