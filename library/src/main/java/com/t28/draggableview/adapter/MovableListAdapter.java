b6b0039ae0960efec8d7f8a84cd7777b63b56362
Tatsuya Maki <t28oogle@gmail.com>
2015-04-15 19:38:53 +0900

    MovableListAdapter追加

---
+++ library/src/main/java/com/t28/draggableview/adapter/MovableListAdapter.java
@@ -0,0 +1,49 @@
+package com.t28.draggableview.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.t28.draggableview.DraggableView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovableListAdapter<T, VH extends RecyclerView.ViewHolder> extends DraggableView.Adapter<VH> {
+    private final List<T> mItems;
+
+    public MovableListAdapter() {
+        this(null);
+    }
+
+    public MovableListAdapter(List<T> items) {
+        mItems = new ArrayList<>();
+        if (items != null || items.size() != 0) {
+            mItems.addAll(items);
+        }
+    }
+
+    @Override
+    public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(VH holder, int position) {
+
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean move(int position1, int position2) {
+        return false;
+    }
+}
