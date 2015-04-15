88d1d2a35c630b9acd43de94d0722a58a21d0b4a
Tatsuya Maki <t28oogle@gmail.com>
2015-04-15 20:20:47 +0900

    ViewHolderの命名変更

--- demo/src/main/java/com/t28/draggableview/demo/data/adapter/AppAdapter.java
+++ demo/src/main/java/com/t28/draggableview/demo/data/adapter/AppAdapter.java
@@ -11,7 +11,7 @@ import com.t28.draggableview.adapter.MovableArrayAdapter;
 import com.t28.draggableview.demo.R;
 import com.t28.draggableview.demo.data.model.App;
 
-public class AppAdapter extends MovableArrayAdapter<App, AppAdapter.ItemViewHolder> {
+public class AppAdapter extends MovableArrayAdapter<App, AppAdapter.ListItemViewHolder> {
     private OnItemLongClickListener mItemLongClickListener;
 
     public AppAdapter() {
@@ -19,14 +19,14 @@ public class AppAdapter extends MovableArrayAdapter<App, AppAdapter.ItemViewHold
     }
 
     @Override
-    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         final View itemView = inflater.inflate(R.layout.layout_list_item, parent, false);
-        return new ItemViewHolder(itemView);
+        return new ListItemViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(ItemViewHolder holder, int position) {
+    public void onBindViewHolder(ListItemViewHolder holder, int position) {
         final App app = getItem(position);
         holder.bind(app);
     }
@@ -39,12 +39,12 @@ public class AppAdapter extends MovableArrayAdapter<App, AppAdapter.ItemViewHold
         void onItemLongClick(int position, View view);
     }
 
-    public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+    public class ListItemViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
         private final ImageView mIconView;
         private final TextView mPrimaryTextView;
         private final TextView mSecondaryTextView;
 
-        public ItemViewHolder(View itemView) {
+        public ListItemViewHolder(View itemView) {
             super(itemView);
             itemView.setOnLongClickListener(this);
             mIconView = (ImageView) itemView.findViewById(R.id.linear_layout_item_icon);
